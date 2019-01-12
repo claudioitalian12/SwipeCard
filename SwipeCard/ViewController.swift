dbf6d3a79960e07120931ea333883fc80bf696d1
claudioitalian12 <dark.claudio12@gmail.com>
2019-01-12 16:16:14 +0100

    Initial Commit

---
+++ SwipeCard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwipeCard
+//
+//  Created by claudio Cavalli on 12/01/2019.
+//  Copyright © 2019 claudio Cavalli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
